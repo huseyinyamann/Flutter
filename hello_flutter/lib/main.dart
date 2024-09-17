eabc010a2a8bf8cd0929e2463201e8f93bc36190
huseyinyamann <141922787+huseyinyamann@users.noreply.github.com>
2024-09-17 17:59:20 +0300

    v1.0.7

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -7,11 +7,11 @@ const url =
     'https://www.snow-forecast.com/system/images/3290/medium/Kandersteg.jpg';
 
 const sundarban =
-'''The Sundarbans mangrove forest, one of the largest such forests in the world (140,000 ha), lies on the delta of the Ganges, Brahmaputra and Meghna rivers on the Bay of Bengal. It is adjacent to the border of India’s Sundarbans World Heritage site inscribed in 1987. The site is intersected by a complex network of tidal waterways, mudflats and small islands of salt-tolerant mangrove forests, and presents an excellent example of ongoing ecological processes. The area is known for its wide range of fauna, including 260 bird species, the Bengal tiger and other threatened species such as the estuarine crocodile and the Indian python.''';
+    '''The Sundarbans mangrove forest, one of the largest such forests in the world (140,000 ha), lies on the delta of the Ganges, Brahmaputra and Meghna rivers on the Bay of Bengal. It is adjacent to the border of India’s Sundarbans World Heritage site inscribed in 1987. The site is intersected by a complex network of tidal waterways, mudflats and small islands of salt-tolerant mangrove forests, and presents an excellent example of ongoing ecological processes. The area is known for its wide range of fauna, including 260 bird species, the Bengal tiger and other threatened species such as the estuarine crocodile and the Indian python.''';
 
 void main() {
   runApp(MyApp());
-  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomePage(),
-        );
+      home: HomePage(),
+    );
   }
 }
 
@@ -35,43 +35,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         title: Text("Home Page"),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          _imageSection(),
-        _headerSection(),
-        _navigationSection(),
-        _bodySection()
-            ],
-          ),
-        ),
-      );
-  }
-
-  Padding _bodySection() {
-    return Padding(padding: const EdgeInsets.all(16.0),
-        child: Text(sundarban),);
-  }
-
-  Padding _navigationSection() {
-    return Padding(
-        padding: const EdgeInsets.only(top: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GenerateNavigationButton(Icons.share, 'SHARE'),
-            GenerateNavigationButton(Icons.route, 'ROUTE'),
-          GenerateNavigationButton(Icons.favorite, 'FAVORITE'),
+            _imageSection(),
+            _headerSection(),
+            _navigationSection(),
+            _bodySection()
           ],
         ),
-      );
+      ),
+    );
   }
 
-   Widget GenerateNavigationButton(IconData _icon, String title){
+  Widget _bodySection() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(sundarban),
+    );
+  }
+
+  Widget _navigationSection() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          GenerateNavigationButton(Icons.share, 'SHARE'),
+          GenerateNavigationButton(Icons.route, 'ROUTE'),
+          GenerateNavigationButton(Icons.favorite, 'FAVORITE'),
+        ],
+      ),
+    );
+  }
+
+  Widget GenerateNavigationButton(IconData _icon, String title) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -95,42 +97,72 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Padding _headerSection() {
+  Widget _headerSection() {
     return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'The Sundarbans',
-                  style: TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Khulna Division Bangadesh',
-                  style: TextStyle(fontSize: 14, color: Colors.grey),
-                )
-              ],
-            ),
-            Spacer(),
-            Icon(
-              Icons.star,
-              color: Colors.amber,
-            ),
-            Text('9.5'),
-          ],
-        ),
-      );
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'The Sundarbans',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Khulna Division Bangadesh',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              )
+            ],
+          ),
+          Spacer(),
+          Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          Text('9.5'),
+        ],
+      ),
+    );
   }
 
-  Image _imageSection() {
-    return Image.network(
-        url,
-        width: double.infinity,
-        height: 200,
-        fit: BoxFit.cover,
-      );
+  Widget _imageSection() {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Image.network(
+          url,
+          width: double.infinity,
+          height: 200,
+          fit: BoxFit.cover,
+        ),
+        Positioned.fill(
+          child: Center(
+            child: Text(
+              'Sunderbans',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -35,
+          left: 0,
+          right: 0,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+                size: 50,
+              ),
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ],
+    );
   }
+
+
 }
