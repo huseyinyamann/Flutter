15acef70059abf90eb2d1f986e51945f008a7fba
huseyinyamann <141922787+huseyinyamann@users.noreply.github.com>
2024-09-18 16:44:29 +0300

    v1.1.0

---
+++ list_view_example/lib/list_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListViewExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> items = List.generate(1000, (index) => 'Item${index +1}');
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List View Example'),),
+          body: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (content, index) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                tileColor:  index.isEven ? Colors.grey : Colors.grey[300],
+                title:  Text('Item ${items[index]}'),
+                subtitle: Text('This is a subtitle ${index +1}'),
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+}
