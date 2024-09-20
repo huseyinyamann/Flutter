4ac268248c3d592a239cb8e2ef43ca26b3e94ec7
huseyinyamann <141922787+huseyinyamann@users.noreply.github.com>
2024-09-20 17:56:55 +0300

    v1.1.6

--- exercise/flutter_exercixe/lib/main.dart
+++ exercise/flutter_exercixe/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_exercixe/101/app_bar_learn.dart';
 import 'package:flutter_exercixe/101/button_learn.dart';
 import 'package:flutter_exercixe/101/container_sized_box_learn.dart';
 import 'package:flutter_exercixe/101/scafold_learn.dart';
@@ -16,26 +18,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: ButtonLearnView(),
+      theme: ThemeData.dark().copyWith(
+          appBarTheme: AppBarTheme(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        actionsIconTheme: IconThemeData(color: Colors.red),
+      )),
+      home: AppBarView(),
     );
   }
 }
