9851018b7d94c96bc0c00b4b8785b8a711552663
huseyinyamann <141922787+huseyinyamann@users.noreply.github.com>
2024-09-24 17:56:57 +0300

    v1.2.0

--- exercise_2/lib/demos/note_demos_view.dart
+++ exercise_2/lib/demos/note_demos_view.dart
@@ -1,71 +1,50 @@
+import 'package:exercise_2/101/image_learn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class NoteDemosView extends StatelessWidget {
   const NoteDemosView({super.key});
 
-  final _title = 'Create your first note';
-  final _description = 'Add a note';
-  final _createNote = 'Create a Note';
-  final _importNote = 'Import Notes';
+    final _title = 'Create your first note';
+    final _description = 'Add a note';
+    final _createNote ='Create a Note';
+    final _importNote ='Import Notes';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-      ),
+      appBar: AppBar(systemOverlayStyle: SystemUiOverlayStyle.dark,),
+
       body: Padding(
         padding: PaddingItems.horizontalPadding,
-        child: Column(
-          children: [
-            PngImage(name: ImageItems().applex),
-            _TitleWidget(title: _title),
-            Padding(
-              padding: PaddingItems.verticalPadding,
-              child: _SubtTitleWidget(
-                title: _description * 10,
-              ),
-            ),
-            const Spacer(), // colum icinde spcer atarsan o tum boslugu alir sadece sana ihtiyacin olan kadar verir
+        child: Column(children: [
+          PngImage(name: ImageItems().applex),
+          _TitleWidget(title: _title),
+          Padding(
+            padding: PaddingItems.verticalPadding,
+            child: _SubtTitleWidget(title: _description *10,),
+          ),
+          const Spacer(), // colum icinde spcer atarsan o tum boslugu alir sadece sana ihtiyacin olan kadar verir
 
-            _createButton(context),
+          _createButton(context),
 
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                _importNote,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge
-                    ?.copyWith(color: Colors.blue),
-              ),
-            ),
-            SizedBox(
-              ButtonHeights.buttonNormalHeight,
-            )
-          ],
-        ),
+           TextButton(onPressed: (){}, child: Text(_importNote, style: 
+           Theme.of(context).textTheme.bodyLarge?.copyWith(
+            color: Colors.blue
+           ),),
+        ),  SizedBox(ButtonHeights.buttonNormalHeight,)],),
       ),
     );
   }
 
   ElevatedButton _createButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: SizedBox(
-        height: ButtonHeights.buttonNormalHeight,
-        child: Center(
-            child: Text(
-          _createNote,
-          style: Theme.of(context).textTheme.headlineSmall,
-        )),
-      ),
-      style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-    );
+    return ElevatedButton(onPressed: (){}, child: SizedBox(height:
+         ButtonHeights.buttonNormalHeight,child: Center(child:
+         Text(_createNote, style: Theme.of(context).textTheme.headlineSmall,)
+         ),
+         ),
+         style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+         );
   }
 }
 
@@ -73,21 +52,13 @@ class NoteDemosView extends StatelessWidget {
 class _SubtTitleWidget extends StatelessWidget {
   const _SubtTitleWidget({
     super.key,
-    this.textAlign = TextAlign.center,
-    required this.title,
+    this.textAlign =TextAlign.center, required this.title,
   });
-  final TextAlign textAlign;
-  final String title;
+final TextAlign textAlign; 
+final String title;
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      textAlign: textAlign,
-      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ),
-    );
+    return Text(title,textAlign: textAlign, style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.black, fontWeight: FontWeight.w500,),);
   }
 }
 
@@ -101,22 +72,16 @@ class _TitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-            color: Colors.black,
-            fontWeight: FontWeight.w700,
-          ),
-    );
+    return Text(title,
+    style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.black, fontWeight: FontWeight.w700,) ,);
   }
 }
 
-class PaddingItems {
-  static final EdgeInsets horizontalPadding =
-      EdgeInsets.symmetric(horizontal: 20);
-  static final EdgeInsets verticalPadding = EdgeInsets.symmetric(vertical: 10);
+class PaddingItems{
+ static final EdgeInsets horizontalPadding = EdgeInsets.symmetric(horizontal: 20);
+ static final EdgeInsets verticalPadding = EdgeInsets.symmetric(vertical: 10);
 }
 
-class ButtonHeights {
-  static const double buttonNormalHeight = 50;
+class ButtonHeights{
+  static const double buttonNormalHeight =50;
 }
---
+++ exercise_3/exercise_3/lib/101/custom_widget_learn_view.dart
@@ -0,0 +1,56 @@
+﻿import 'package:flutter/material.dart';
+
+class CustomWidgetLearnView extends StatelessWidget {
+  const CustomWidgetLearnView({super.key});
+  final String title = 'Food';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            CustomFootButton(title: 'Food'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ColorsUtility {
+  final Color redColor = Colors.red;
+  final Color whiteColor = Colors.white;
+}
+
+class CustomFootButton extends StatelessWidget {
+  const CustomFootButton({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _ColorsUtility().redColor,
+        shape: const StadiumBorder(),
+      ),
+      onPressed: () {},
+      child: Center(
+        child: Padding(
+          padding: _PaddingUtility().normalPadding,
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: _ColorsUtility().whiteColor,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PaddingUtility {
+  final normalPadding = const EdgeInsets.all(8.0);
+  final normal2xPadding = const EdgeInsets.all(8.0);
+}
